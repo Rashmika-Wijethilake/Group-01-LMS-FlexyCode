b635769ca609675ce919a6389c079872720c476e
madhushansilva123 <104122547+madhushansilva123@users.noreply.github.com>
2023-09-13 08:01:21 +0530

    Update forgot password.js

--- src/pages/forgot password.js
+++ src/pages/forgot password.js
@@ -1,2 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
+export default function Forgot_Password() {
+    return ( < div >
+
+        <
+        div >
+        <
+        h1 > Enter the Email address associated with your account and we 'll srnd you code to reset your password. </h1> </div >
+
+        <
+        form >
+
+        <
+        input type = "email"
+        id = "email"
+        name = "email"
+        placeholder = 'Enter your email address' > < /input><br/ > < form br / >
+
+
+        <
+        /form>
+
+
+        <
+        button > Continue < /button>
+
+        <
+        /div>
+
+
+
+    )
+
+
+}
