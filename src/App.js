08d8c6b346ef2acc3b6423d89d2363977ee0ccc6
enoshlivera <enoshdilshan2005@gmail.com>
2023-09-14 09:36:07 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import {BrowserRouter, Routes, Route  } from 'react-router-dom';
-import SignUp from './pages/SignUp';
-
-
+import React from 'react';
+import './App.css';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-      <Routes>
-        <Route index element={<SignUp/>}/>       
-        
-
-      </Routes>
-      </BrowserRouter>
+      <Login />
     </div>
   );
 }
