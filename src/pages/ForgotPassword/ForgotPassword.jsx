e3a42dde710d6c0cd66dc50fe724826fd9d6fee4
mino2001 <shadeenaminoshi@gmail.com>
2023-09-16 22:36:30 +0530

    adding css styles to forgot password

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../ForgotPassword/ForgotPassword.css';
 
 export default function ForgotPassword() {
     return ( 
-    <div>
-            <div className="head">
-                <h4 > Enter the Email address associated with your account, and we 'll send you a code to reset your password.</h4>
-             </div>
+        <div className='wrapper bg-dark d-flex align-items-center justify-content-center w-100'>
+        <div className="forgot-password">
+          <h5 className='heading mb-4'>Enter the email address associated with your account and we'll sent you a code to reset your password</h5>
+  
+          <form className='need-validation'>
+              <div className='form-group was-validated mb-4'>
+                <div className="Email">
+                  <input type="email" id="email" name="email" placeholder="Email Address" className='form-control' required></input>
+                  <div className='invalid-feedback'>
+                      Enter valid email address
+                  </div>
+                </div>
+              </div>
 
-            <div className='Email'>
-                    <input type='email'
-                        id='email'
-                        name='email'
-                        placeholder='Enter your email address'> </input><br></br>
-            </div>
-
-            <div>
-            <button type="submit">Continue</button> 
-            </div>
-
-    </div>
+              <button type="submit" className='btn-continue btn btn-info w-100 mt-2'>Continue</button>
+          </form>
+        </div>
+      </div>
 
     );
 }
